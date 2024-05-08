e5551a37cd1bc40bb169627cd045f4cadabb0182
Soulaimane Yahya <soulaimaneyh07@gmail.com>
2024-05-08 16:18:34 +0100

    fixing

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt){
+    // configuration
+    grunt.initConfig({
+        // pass in options tu plugins, references to files etc
+        concat: {
+            css: {
+                src: ['assets/css/*.css'],
+                dest: 'build/style.min.css'
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'build/style.min.css': ['build/style.min.css']
+                }
+            }
+        }
+    })
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-concat')
+    grunt.loadNpmTasks('grunt-contrib-cssmin')
+
+    // register tasks
+    grunt.registerTask('concat-css', ['concat:css']);
+};
---
+++ README.md
@@ -0,0 +1,15 @@
+# Multividas Status
+
+Multividas systems operations status
+
+```grunt build
+nvm use v18 &&  grunt concat-css
+nvm use v18 &&  grunt cssmin
+```
+
+----- 
+Need helps? Reach me out
+
+> Email: multividasdotcom@gmail.com
+
+All the best :beer:
